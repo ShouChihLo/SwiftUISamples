d5049fdd9057b7df740dacaaaa16c89a6335b415
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2023-10-17 10:34:26 +0800

    Add files via upload

---
+++ NavigationTest/NavigationTest/NavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestApp.swift
+//  NavigationTest
+//
+//  Created by 羅壽之 on 2023/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
