43cd6513fb4fa81661b8fa257ec578d9570ae725
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2023-10-25 09:18:41 +0800

    Add files via upload

---
+++ ScrollViewTest/ScrollViewTest/ScrollViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTestApp.swift
+//  ScrollViewTest
+//
+//  Created by 羅壽之 on 2023/3/28.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
