31c8e8abfc528a9e033b754839f3aa52ca22676d
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2023-11-28 11:15:08 +0800

    Add files via upload

---
+++ AnimationTest/AnimationTest/AnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestApp.swift
+//  AnimationTest
+//
+//  Created by 羅壽之 on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
