f5a0661b5c5e671eb835a617ee66dd8e55cb94e2
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2024-07-04 15:46:17 +0800

    Add files via upload

---
+++ ListTest/ListTest/Restaurant.swift
@@ -0,0 +1,13 @@
+//
+//  Restaurant.swift
+//  ListTest
+//
+//  Created by 羅壽之 on 2023/1/16.
+//
+
+import Foundation
+
+struct Restaurant: Hashable {
+    var name: String = ""
+    var image: String = ""
+}
