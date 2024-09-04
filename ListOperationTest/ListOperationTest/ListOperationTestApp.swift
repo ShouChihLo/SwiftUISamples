55d1c580e65d49c928c56baf72449466804fda85
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2024-09-04 10:00:10 +0800

    Add files via upload

---
+++ ListOperationTest/ListOperationTest/ListOperationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOperationTestApp.swift
+//  ListOperationTest
+//
+//  Created by 羅壽之 on 2023/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ListOperationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
