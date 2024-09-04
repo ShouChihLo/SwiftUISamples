30bf9318339161bb2cdd96f4815a200eb827549f
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2024-09-04 09:57:36 +0800

    Add files via upload

---
+++ InputTest/InputTest/InputTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputTestApp.swift
+//  InputTest
+//
+//  Created by 羅壽之 on 2023/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InputTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+           ContentView()
+        }
+    }
+}
---
+++ InputTest/InputTest/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  InputTest
+//
+//  Created by 羅壽之 on 2023/8/30.
+//
+
+import Foundation
+
+struct Profile {
+    var name = ""
+    var bloodIndex = 0
+    var birthday = Date()
+    var frequency = 0
+    var smoke = false
+}
